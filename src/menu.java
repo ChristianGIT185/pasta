610326168ba7783d5f6b5a45d2be631945ef0282
ChristianGIT185 <moiak900@gmail.com>
2026-09-15 12:11:02 -0300

    Adiciona programa Java

---
+++ src/menu.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class menu {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("User, Input number to continue:");
+        int num = scanner.nextInt();
+
+        System.out.println("The number is: " + num);
+        scanner.close();
+
+
+
+
+    }
+    
+}
